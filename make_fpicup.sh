82289d7a09628b5dbe009ff29c40b9278baa2657
uhmin <uhmin@gen-info.osaka-u.ac.jp>
2013-02-22 15:49:55 +0900

    Create make_fpicup.sh

---
+++ make_fpicup.sh
@@ -0,0 +1,3 @@
+gcc -Wall -O -D_FILE_OFFSET_BITS=64 -o fpicup_linux.5.2.exe        fpicup5.2.100420.c
+gcc -Wall -O -D_FILE_OFFSET_BITS=64 -o fpicmakeindex_linux.exe     fpicmakeindex.100618.c
+gcc -Wall -O -D_FILE_OFFSET_BITS=64 -o fpicmakebindex4.1_linux.exe fpicmakebindex4.1.2.c
