4ca95c0bba47738e91f5fc8a3c1558eebabe6d2c
uhmin <uhmin@gen-info.osaka-u.ac.jp>
2014-06-16 14:49:42 +0900

    Update make_fpicup.sh

--- make_fpicup.sh
+++ make_fpicup.sh
@@ -1,6 +1,8 @@
 gcc -Wall -O -D_FILE_OFFSET_BITS=64 -o fpicup_linux.5.2.exe        fpicup5.2.100420.c
 gcc -Wall -O -D_FILE_OFFSET_BITS=64 -o fpicmakeindex_linux.exe     fpicmakeindex.100618.c
 gcc -Wall -O -D_FILE_OFFSET_BITS=64 -o fpicmakebindex4.1_linux.exe fpicmakebindex4.1.2.c
+
 ln -s fpicmakeindex_linux.exe fpicmakeindex.exe
 ln -s fpicmakebindex4.1_linux.exe fpicmakebindex4.exe
 ln -s fpicup_linux.5.2.exe fpicup.exe
+chmod +x fpicupmakeindex.sh *.pl
