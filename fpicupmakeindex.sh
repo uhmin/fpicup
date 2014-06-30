9f31ce5ffd9b27b18e14ffc2827021875f28b272
uhmin <uhmin@gen-info.osaka-u.ac.jp>
2014-06-30 16:03:28 +0900

    Update fpicupmakeindex.sh

--- fpicupmakeindex.sh
+++ fpicupmakeindex.sh
@@ -1,2 +1,4 @@
-fpicmakeindex.exe ${1}
-fpicmakebindex4.exe < $1.index >$1.bindex
+#!/bin/bash
+myDIR=$(cd $(dirname $0) && pwd)
+$myDIR/fpicmakeindex.exe ${1}
+$myDIR/fpicmakebindex4.exe < $1.index >$1.bindex
